498147a209145b631d85b27c37120c1c1ac92f4a
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-10-01 11:48:11 -0300

    adicionando visualização ao home

--- js/controller/Home.controller.js
+++ js/controller/Home.controller.js
@@ -2,11 +2,37 @@
 (function () {
     var app = angular.module('cdbApp');
 
-    app.controller("HomeController", function HomeController($state) {
+    app.controller("HomeController", function HomeController($state, VizService) {
 
         var homeCtrl = this;
+        const STATE_BUSCA = "cidadao-de-bolso.busca";
 
-        homeCtrl.test = "TESTE DO CONTROLLER";
+        VizService.getTags().then(
+            function success(response) {
+                console.log(response.slice(0, 8))
+                var data = response.slice(0, 8)
+                const viz = d3plus.viz()
+                    .container("#treemap")
+                    .data(data)
+                    .id("name")
+                    .size("count")
+                    .margin(1)
+                    .font({
+                        "family": "HelveticaNeue",
+                        "transform": "uppercase"
+
+                    })
+                    .mouse({
+                        "move": false,
+                        "click": buscarPorTag
+                    })
+                    .draw();
+            }
+        );
+
+        function buscarPorTag (tag) {
+            $state.go(STATE_BUSCA, {tag: tag.name})
+        }
 
     });
 })();
--- js/service/Visualization.service.js
+++ js/service/Visualization.service.js
@@ -6,16 +6,57 @@
 
         var service = this;
 
+        /**
+         * {
+         *   id, type, count
+         * }
+         */
+
         service.getTags = () => {
 
+            var deffered = $q.defer();
             PostService.loadQuestions().then(
                 function success(questionsJSON) {
-                    var tags = arrayOfTags(questionsJSON);
-
+                    var popularTags = mostPopularTags(questionsJSON);
+                    deffered.resolve(popularTags);
+                }, function exception(erro) {
+                    deffered.reject(erro);
                 });
+            return deffered.promise;
         };
 
-        function arrayOfTags(questionsJSON) {
+        function mostPopularTags(questionsJSON) {
+            var tags = concatenateAndCleanInvalidTags(questionsJSON);
+            var frequencyDict = frequencyOfTags(tags);
+            var listOfTags = []
+            var count = 0; 
+            for (var i in frequencyDict){
+                listOfTags[count] = {
+                    'id':count, 
+                    'name': i,
+                    'count':frequencyDict[i]
+                };
+                count++;
+            }
+            return listOfTags.sort((a,b)=> {
+                return b.count - a.count;
+            });
+        }
+
+        function compareCounts(a,b) {
+            return 
+        }
+
+        function frequencyOfTags(listOfTags) {
+
+            var uniques = {}
+            for (var i = 0; i < listOfTags.length; i++) {
+                uniques[listOfTags[i]] = 1 + (uniques[listOfTags[i]] || 0);
+            }
+            return uniques
+        }
+
+        function concatenateAndCleanInvalidTags(questionsJSON) {
             var tags = questionsJSON.reduce((acc, val) => {
                 return acc.concat(val.tags);
             }, []);
@@ -24,7 +65,5 @@
                 return val !== "";
             });
         };
-
-
     });
 })();
